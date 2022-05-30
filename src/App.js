32244d28cf9a1138b951494a674785ffb10a5f93
MichalTrostowiecki <michal.trostowiecki@gmail.com>
2022-05-30 19:15:12 +0100

    I've added state that get refreshed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Component } from 'react';
 
 
 
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -10,23 +10,37 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Michal'
-    }
+      monsters: [],    
+    };
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(
+      () => {
+        return {monsters: users}
+      },
+      () => {
+        console.log(this.state);
+      }
+      ))   
+  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button onClick={() => {
-            this.setState({name: 'Andrei'});
-          }}>Change Name</button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            );
+          })
+        }
       </div>
-    );
+    )
   }
 }
 
